436ad6211e3e997f49baacaa417aa823ba90fbb0
Sahil Gupta <sahilgupta70500@gmail.com>
2025-03-12 22:59:59 -0400

    Update Contact-Me.jsx

--- src/pages/Contact-Me.jsx
+++ src/pages/Contact-Me.jsx
@@ -19,7 +19,7 @@ function Contact() {
                 <label for="name" className="contact_labels"> Your Name:</label>
                 <input required id="name" size={60} minLength={2} maxLength={100} name="name" placeholder="Alex Cola" type="text" />
                 <label for="email" className="contact_labels"> Your Email:</label>
-                <input id="email" size={60} minLength={7} name="email" required maxLength={15} type="email" placeholder="alexcol90239@gmail.com" />
+                <input id="email" size={60} minLength={7} name="email" required maxLength={25} type="email" placeholder="alexcol90239@gmail.com" />
                 <label for="description" className="contact_labels"> Description:</label>
                 <textarea rows={15} required cols={80} id="description" name="description" placeholder="Hi Sahil, hope your are doing well. I saw you porfolio and would like to know more about your expertise...."></textarea>
                 <div className="submit_reset_div">

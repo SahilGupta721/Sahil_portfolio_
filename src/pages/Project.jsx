ee00012cabbe13997265ea09661d5b00aa0a77b5
Sahil Gupta <sahilgupta70500@gmail.com>
2025-07-07 19:47:19 -0400

    add project

--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -6,9 +6,6 @@ import Navbar from '../components/Navbar/navbar.jsx';
 import "../css/project.css";
 import '../components/Navbar/navbar.css';  
 
-const winter = 'https://res.cloudinary.com/doype8ygx/video/upload/v1741510586/exxaa6aamaq3nkad9lfv.mp4';
-const interviewprep_video = 'https://res.cloudinary.com/doype8ygx/video/upload/v1741754142/interview_jyguxc.mp4';
-
 function Projects() {
     return (
         <>
@@ -19,8 +16,18 @@ function Projects() {
             <div id="project_content">
                 <section className="section_project_content">
                     <div id="project_cards">
+                        <div id="project_five">
+                            
+                            <span className='project-name'>EcoSphere</span>
+                            <div className="buttons_five">
+                                <a href='https://www.eco-sphere.co/' target='_blank'><span id='live'>Live</span></a>
+                                
+                                <NavLink to='https://github.com/sarahhalime/EcoSphere' target='_blank'><span id='code'>Code</span></NavLink>
+                            </div>
+                        </div>
+                        
                         <div id="project_one">
-                            <video src={interviewprep_video} autoPlay loop muted className="interviewprep-video" />
+                            
                             <span className='project-name'>InterviewPrep</span>
                             <div className="buttons_one">
                                 <a href='https://interviewprep-ddne.onrender.com/' target='_blank'><span id='live'>Live</span></a>
@@ -29,7 +36,7 @@ function Projects() {
                             </div>
                         </div>
                         <div id="project_two">
-                            <video src={winter} autoPlay loop muted className="winter-video" />
+                            
                             <span className='project-name'>VizWeather</span>
                             <div className="buttons_two">
                             <NavLink to='https://vizweather.vercel.app/' target='_blank'>  <span id='live_two'>Live</span></NavLink>
@@ -53,6 +60,8 @@ function Projects() {
                             <NavLink to='https://github.com/SahilGupta721/Gui-Calculator' target='_blank'><span id='code_four'>Code</span>
                             </NavLink>
                         </div>
+                        
+                        
                     </div>
                 </section>
             </div>
